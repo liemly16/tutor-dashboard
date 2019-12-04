37c082520d019cf860568ae41d1463aebc54be35
wever0408 <39722379+wever0408@users.noreply.github.com>
2019-12-04 10:44:58 +0700

    Add login & register page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,43 @@
-import React from 'react'
-import Header from '../containers/HeaderContainer'
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+// eslint-disable-next-line camelcase
+import jwt_decode from 'jwt-decode';
+import setAuthToken from '../utils/setAuthToken';
+import store from '../store';
+import { setCurrentUser, logoutUser } from '../actions/authActions';
+import Register from '../containers/auth/Register';
+import Login from '../containers/auth/Login';
+import Navbar from "./NavBar";
+
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+  // Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+
+    // Redirect to login
+    window.location.href = './login';
+  }
+}
 
 const App = () => (
-  <div>
-    <Header />
-  </div>
-)
+  <Router>
+    <div className="App">
+    <Navbar />
+      {/* <Route exact path="/" component={MainPage} /> */}
+      <Route exact path="/register" component={Register} />
+      <Route exact path="/login" component={Login} />
+    </div>
+  </Router>
+);
 
-export default App
+export default App;
--- client/src/constants/ActionTypes.js
+++ client/src/constants/ActionTypes.js
@@ -1,1 +1,5 @@
-export const ADD_TODO = 'ADD_TODO'
+export const GET_ERRORS = "GET_ERRORS";
+export const USER_LOADING = "USER_LOADING";
+export const SET_CURRENT_USER = "SET_CURRENT_USER";
+export const SET_ERROR = "SET_ERROR";
+export const SYSTEM_ERROR = "Hệ Thống Có Lỗi. Vui Lòng Thử Lại Sau";
